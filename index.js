e9919d038134e704e2e892d076de96f8e7552e7b
Nicholas Liu <nickliu96@gmail.com>
2017-11-19 15:41:31 -0500

    Add search REST call for Spotify

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const request = require('request');
+const qs = require('querystring');
+const app = express();
 
 require('dotenv').config()
 
 app.get('/', (req, res) => res.send(process.env.CLIENT_ID))
 
-app.listen(3001, () => console.log('Example app listening on port 3001!'))
+app.get('/search', (req, res) => {
+  var options = {
+    url: 'https://api.spotify.com/v1/search',
+    qs: { q:req.query.q, type:req.query.type },
+    headers: { 'Authorization': 'Bearer ' + process.env.ACCESS_TOKEN },
+    json: true
+  };
+  var search = request.get(options, (error, response, body) => {
+    if(!error) {
+      return res.send(body);
+    } else {
+      console.log(error);
+    }
+  });
+})
+
+app.listen(3001, () => console.log('App listening on port 3001!'))
