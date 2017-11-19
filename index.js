2392da5553cf7b768d1c289cd73fff36328134fc
Nicholas Liu <nickliu96@gmail.com>
2017-11-19 17:00:43 -0500

    Add artist REST call

--- index.js
+++ index.js
@@ -23,4 +23,20 @@ app.get('/search', (req, res) => {
   });
 })
 
+app.get('/artists', (req, res) => {
+  var options = {
+    url: 'https://api.spotify.com/v1/artists/' + req.query.id,
+    headers: { 'Authorization': 'Bearer ' + process.env.ACCESS_TOKEN },
+    json: true
+  };
+
+  request.get(options, (error, response, body) => {
+    if(!error) {
+      return res.send(body);
+    } else {
+      console.log(error);
+    }
+  });
+})
+
 app.listen(3001, () => console.log('App listening on port 3001!'))
